265af6d7eca0d9db9baa551f0b0abd2135877a72
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-04 06:30:10 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser so it won't work
+
+console.log(3
+    + 
+    3) // code readability should be high
+
+console.log("Swasti")
+
+/* 
+documentation of JS one is mdn and another is tc.39.es (ecma262)
+*/
+
+let name = "Swasti"
+let age = 22
+let isLoggedIn = false 
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object 
+
+// typeof => which datatype it belongs to
+console.log(typeof"Swasti");
+console.log(typeof age);
+console.log(typeof null); // o/p :- object
+console.log(typeof undefined); // o/p :- undefined
