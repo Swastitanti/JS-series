416bde4b0e718f59e200fa5fb06daac1930f76e6
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-04 07:50:33 +0000

    conversionOfDatatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,29 @@
-// let score = "33abc"
-// let score1 = null 
-// let score2 = undefined
-// let score3 = true
+let score = "33abc"
+let score1 = null 
+let score2 = undefined
+let score3 = true
 
-// console.log(typeof (score));
+console.log(typeof (score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); // o/p :- string
-// console.log(valueInNumber); // o/p :- NaN (not a number)
-// console.log(score1); // o/p :- 0
-// console.log(score2); // o/p :- NaN
-// console.log(score3); // o/p :- 1; if false than 0
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); // o/p :- string
+console.log(valueInNumber); // o/p :- NaN (not a number)
+console.log(score1); // o/p :- 0
+console.log(score2); // o/p :- NaN
+console.log(score3); // o/p :- 1; if false than 0
 
-// let isLoggedIn = 1 
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
-// console.log(booleanIsLoggedIn);
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
 
-// // 1 => true; 0 => false
-// // "" => false
-// // "Swasti" => true; after conversion to boolean
+// 1 => true; 0 => false
+// "" => false
+// "Swasti" => true; after conversion to boolean
 
-// let someNumber = 33
-// let stringNumber = String(someNumber);
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
 
 // ******************** Operstions ********************
 
@@ -31,12 +31,12 @@ let value = 3
 let negValue = -value 
 // console.log(negValue);
 
-// console.log(2 + 2); // addition
-// console.log(2 - 2); // subtraction
-// console.log(2 * 2); // multiplication
-// console.log(2 ** 3); // 2 to power 3 
-// console.log(4/2); // division
-// console.log(4 % 2); // modulo
+console.log(2 + 2); // addition
+console.log(2 - 2); // subtraction
+console.log(2 * 2); // multiplication
+console.log(2 ** 3); // 2 to power 3 
+console.log(4/2); // division
+console.log(4 % 2); // modulo
 
 
 let str1 = "hello"
