3b6c4ad34184cf47ac14f5cba1ce46225d182b5c
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-04 08:57:18 +0000

    dataTypeAndMemory

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -35,3 +35,26 @@ console.log(typeof myFunction); // o/p:- function
 // javascript is dynamically typed language 
 
 // link :- https://tc39.es/ecma262/multipage/ecmascript-language-expressions.html#sec-typeof-operator
+
+
+// ************************* Memory *************************
+
+// two type of Memories Stack (primitive) and Heap (Non-primitive)
+
+let myName = "swastiKumari"
+let anotherName = myName
+anotherName = "kumariSwasti"
+
+console.log(myName); // o/p :-  swastiKumari
+console.log(anotherName); // o/p :- kumariSwasti
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@sbi"
+}
+
+let userTwo = userOne 
+userTwo.email = "swasti@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
