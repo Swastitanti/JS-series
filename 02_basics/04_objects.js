5624aebbab97245b5810f1fa3d774b8152ae9e76
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-10 17:16:17 +0000

    objects_part2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,7 +30,7 @@ const obj2 = {3: "c", 4: "d"}
 const obj3 = Object.assign({}, obj1, obj2)
 console.log(obj3); // o/p :- { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
-// in general we use separator(...)
+// in general we use spread(...)
 const obj4 = {...obj1, ...obj2}
 console.log(obj4); // o/p :- { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
