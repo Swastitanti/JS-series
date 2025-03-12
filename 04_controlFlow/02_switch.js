5f5dfded30e65abfe4b4b254b9cd74900aecd1b8
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-12 12:56:11 +0000

    switch

---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,36 @@
+// switch Syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 3 //"jan"
+// similarly for strings
+switch (month) {
+    case 1: // we can write "jan" instead of 1
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("Default case");
+        break;
+}
+
+
