d6c49d3832d8bddceacda5a8cf64169edd48ebb5
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-12 17:10:49 +0000

    doWhileLoops

---
+++ 05_iterations/02_whileLoop.js
@@ -0,0 +1,29 @@
+// while Loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2 // terminate value
+}
+
+let myArray = ['flash', "batnam", "superman"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+
+
+// Do while loop
+// it execute atleast one time
+let scores = 11
+do {
+    console.log(`score is ${scores}`);
+    scores++
+} while (scores <= 10);
