bd3eff5712408f8e09c9d91ce8b81d15b3128df3
Swasti Kumari <120382838+Swastitanti@users.noreply.github.com>
2025-03-18 17:24:34 +0000

    SyncAndAsync

---
+++ 08_events/02_two.js
@@ -0,0 +1,21 @@
+/*
+=> JavaScript 
+    1. synchronous language
+    2. single Thresded (javascript always in runtime environment)
+    3. its all about default javascript
+=> execution context
+    1. execute one line of code at a time
+        single thread way
+        1. console log-1
+        2. console log-2
+        3. CALL stack
+        4. Memory Heap
+        (each operation waits for the last one to complete before executing)
+=> Two typw of code written in JS
+    1. Blocking Code
+        => Block the flow of program
+        => read file synchronized
+    2. Non-Blocking Code
+        => Does not block execution
+        => read file Asynchronized 
+*/
